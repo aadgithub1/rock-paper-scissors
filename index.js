b33133b908ab1b75b0494c48d99716bb2dab835f
Aaron <152732836+aadgithub1@users.noreply.github.com>
2024-07-22 09:36:23 -0400

    Fix logic and properly display results

--- index.js
+++ index.js
@@ -1,18 +1,72 @@
 let humanScore = 0;
+let humanDisplayText = document.querySelector('#human-display-text')
 let compScore = 0;
+let computerDisplayText = document.querySelector('#computer-display-text')
+let result = document.querySelector('.round-result')
 let selectDiv = document.querySelector('.select-div')
 let selectorBtns = document.querySelectorAll('button')
 
 selectorBtns.forEach((btn) => btn.addEventListener('click', function(){
-    playRound(btn.textContent, getCompChoice())
+    let humanSelection = btn.textContent.toLowerCase()
+    let computerSelection = getCompChoice()
+
+    let winner = decideWinner(humanSelection, computerSelection) 
+    if (winner === 1){
+        humanWinsActions()
+    } else if (winner === 0){
+        compWinsActions()
+    } else if (winner === -1){
+        tieActions()
+    }
+    checkScore(5) //checks to see if either player has reached 5
 }))
-
-//
-
-
-// Display the running score, and announce a winner of the game once one player reaches 5 points
-
-
+function decideWinner(humanScore, compScore){
+    if (humanScore !== compScore){
+        switch(humanScore){
+            case 'rock':
+                if (compScore === 'scissors'){
+                    return 1
+                }
+                return 0
+            case 'paper':
+                if (compScore === 'rock'){
+                    return 1
+                }
+                return 0
+            case 'scissors':
+                if (compScore === 'paper'){
+                    return 1
+                }
+                return 0
+        }
+    }
+    return -1
+}
+function checkScore(maxScore){
+    if (humanScore === maxScore || compScore === maxScore){
+        selectorBtns.forEach((btn) => btn.disabled = true)
+        printFinalResult()
+    }
+}
+function printFinalResult(){
+    if (humanScore > compScore){
+        return result.textContent = 'You won the whole game!'
+    }
+    return result.textContent = 'You lost the whole game!'
+}
+function humanWinsActions(){
+    humanScore++
+    humanDisplayText.textContent = `Your Score: ${humanScore}`
+    result.textContent = 'You won the round!'
+}
+function compWinsActions(){
+    compScore++
+    computerDisplayText.textContent = `Computer Score: ${compScore}`
+    result.textContent = 'You lost the round!'
+}
+function tieActions(){
+    result.textContent = 'It was a tie'
+}
 function getCompChoice(){
     //generate a random number from 1 to 3
     let randomSelector = Math.floor(Math.random() * 3) + 1;
@@ -27,55 +81,4 @@ function getCompChoice(){
         return "scissors"
     }
     //result: return rock, paper, or scissors
-}
-
-
-function playRound(humanChoice, compChoice){
-    if (humanChoice !== compChoice){
-        switch(humanChoice.toLowerCase()){
-            case 'rock':
-                if (compChoice === 'paper'){
-                    printHumanLosesMsgAndIncScore(humanChoice, compChoice);
-                } else if (compChoice === 'scissors'){
-                    printHumanWinsMsgAndIncScore(humanChoice, compChoice);
-                }
-                break
-            case 'paper':
-                if (compChoice === 'rock'){
-                    printHumanWinsMsgAndIncScore(humanChoice, compChoice);
-                } else if (compChoice === 'scissors'){
-                    printHumanLosesMsgAndIncScore(humanChoice, compChoice);
-                }
-                break
-            case 'scissors':
-                if (compChoice === 'rock'){
-                    printHumanLosesMsgAndIncScore(humanChoice, compChoice);
-                } else if (compChoice === 'paper'){
-                    printHumanWinsMsgAndIncScore(humanChoice, compChoice);
-                }
-                break
-        }
-    } else{
-        printTieMessage();
-    }
-
-//result: log win, loss, or tie message
-}
-
-function printHumanWinsMsgAndIncScore(humanChoice, compChoice){
-    let msg = document.createElement('h2')
-    msg.textContent = `You won! ${humanChoice} beats ${compChoice}`
-    selectDiv.appendChild(msg)
-
-    humanScore++;
-}
-function printHumanLosesMsgAndIncScore(humanChoice, compChoice){
-    let msg = document.createElement('h2')
-    msg.textContent = `You won! ${humanChoice} beats ${compChoice}`
-    selectDiv.appendChild(msg)
-
-    compScore++;
-}
-function printTieMessage(){
-    console.log('Great minds think alike, it\'s a tie!');
 }
